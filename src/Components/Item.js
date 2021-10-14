4dce3f5fb7e2dd47a2206e74c9a1a28d98a26def
ArashMalekian <malekianarash.am@gmail.com>
2021-10-14 16:36:38 +0330

    complete stylesheet

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -8,13 +8,14 @@ export const Item = (props) => {
     const {image , name , symbol , current_price , market_cap , high_24h , low_24h} = props.data
     return (
         <div className={styles.container} >
+            <h3>{symbol.toUpperCase()}</h3>
             <img src={image} alt ="imagec" />
             <h1>{name}</h1>
-            <h4>{current_price.toLocaleString()} $</h4>
+            <h4> $ {current_price.toLocaleString()} </h4>
             <h4>{market_cap.toLocaleString() }</h4>
             <div className={styles.pcontainer} >
-            <h2 className={styles.high}  >&#8593; {high_24h} $</h2>
-            <h2 className={styles.low} >&#8595; {low_24h} $</h2>
+            <h2 className={styles.high}  >$ {high_24h} &#8593; </h2>
+            <h2 className={styles.low} >$ {low_24h} &#8595;</h2>
             </div>
 
 
